1595fb4696c6832530249abffd99e8f24d136cd8
flaviodorta <dorta.dev@gmail.com>
2022-06-28 15:18:50 -0300

    feat: added breakpoint object in theme file

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,35 +1,50 @@
 import { DefaultTheme } from 'styled-components';
 
+export const breakpoints = {
+  sm: '480px',
+  md: '768px',
+  lg: '976px',
+  xl: '1440px',
+};
+
 const base = {
-  secondary: {
-    one: '#FFFFFF',
-    two: '#ECECEC',
-    three: '#F9F9F9',
+  colors: {
+    secondary: {
+      one: '#FFFFFF',
+      two: '#ECECEC',
+      three: '#F9F9F9',
+    },
+
+    red: '#ff4c30',
+    orange: '#e67e22',
+    yellow: '#f5e51b',
+    blue: '#284387',
+    purple: '#a537fd',
   },
-  red: '#ff4c30',
-  orange: '#e67e22',
-  yellow: '#f5e51b',
-  blue: '#284387',
-  purple: '#a537fd',
 };
 
 export const light: DefaultTheme = {
   mode: 'light',
+
   colors: {
-    ...base,
+    ...base.colors,
+
     primary: {
       one: '#00e640',
       two: '#2ecc71',
       three: '#93faa5',
     },
+
     font: '#030303',
   },
 };
 
 export const dark: DefaultTheme = {
   mode: 'dark',
+
   colors: {
-    ...base,
+    ...base.colors,
+
     primary: {
       one: '#121212',
       two: '#181818',
