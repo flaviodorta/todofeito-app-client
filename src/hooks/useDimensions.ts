d1d02aed13c36672aa38aeb26f1ffd7a320b3678
flaviodorta <dorta.dev@gmail.com>
2022-06-30 16:09:41 -0300

    feat: patched useDimensions to listen window resizes

--- src/hooks/useDimensions.ts
+++ src/hooks/useDimensions.ts
@@ -1,10 +1,31 @@
-import { Dispatch, SetStateAction, useCallback, useLayoutEffect, useState } from 'react';
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useLayoutEffect,
+  useState,
+} from 'react';
+import { useEventListener } from './useEventListener';
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 
-type Response<T> = [Dispatch<SetStateAction<T | null>>, DOMRect | {}];
+type Response<T> = [Dispatch<SetStateAction<T | null>>, DOMRect];
+
+const DOMRectNull = {
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+} as DOMRect;
 
 export function useDimensions<T extends HTMLElement>(): Response<T> {
-  const [dimensions, setDimensions] = useState<DOMRect | {}>({});
+  const [dimensions, setDimensions] = useState<DOMRect>(DOMRectNull);
   const [ref, setRef] = useState<T | null>(null);
+
+  const { x, y, width, height, top, right, bottom, left } = dimensions;
 
   const handleDimensions = useCallback(() => {
     if (ref) setDimensions(ref.getBoundingClientRect());
@@ -12,7 +33,9 @@ export function useDimensions<T extends HTMLElement>(): Response<T> {
 
   useLayoutEffect(() => {
     handleDimensions();
-  }, [ref]);
+  }, [x, y, width, height, top, right, bottom, left]);
+
+  useEventListener('resize', handleDimensions);
 
   return [setRef, dimensions];
 }
