4a07b14367718149c9d39c19dcc34768f8d70ca5
flaviodorta <dorta.dev@gmail.com>
2022-07-14 23:37:55 -0300

    fix: cleaned user, todo, proejct, comment, activity interfaces

--- src/store/slice/initialState.types.ts
+++ src/store/slice/initialState.types.ts
@@ -1,48 +1,58 @@
-export interface User {
-  id: string;
-  email: string;
-  theme: string;
-  photo: string;
-  name: string;
-  password: string;
+export type userId = string;
+export type todoId = string;
+export type projectId = string;
+export type commentId = string;
+export type activityId = string;
+
+export interface InitialState {
+  user: IsUser | null;
+  todos: IsTodo[];
+  projects: IsProject[];
 }
 
-export interface Todo {
-  id: string;
-  owner: User;
-  title: string;
+export interface IsUser {
+  userId: string;
+  email: string;
+  password: string;
+  name?: string;
+  lastName?: string;
+  photo?: string;
+}
+
+export interface IsTodo {
+  todoId: string;
+  createdBy: userId;
+  name: string;
   content: string;
   date: Date;
   isCompleted: boolean;
-  sharedUsers: boolean;
-  labels: string[];
-  reminder: string;
-  project?: string;
+  comments?: IsComment[];
+  priority?: 1 | 2 | 3 | 4;
+  sharingUsers?: userId[];
+  labels?: string[];
+  reminder?: Date;
+  project?: projectId;
 }
 
-export interface Comment {
-  id: string;
-  user: User;
+export interface IsProject {
+  projectId: string;
+  createdBy: userId;
+  todos?: todoId[];
+  sharingUsers?: userId[];
+  comments?: IsComment[];
+  activities?: IsActivity[];
+}
+
+export interface IsComment {
+  commentId: string;
+  createdBy: userId;
   content: string;
   date: Date;
 }
 
-export interface Activity {
-  user: User;
+export interface IsActivity {
+  activityId: string;
+  createdBy: userId;
   content: string;
   date: Date;
-}
-
-export interface Project {
-  todos: Todo[];
-  owner: User;
-  collaborators: User[];
-  comments: Comment[];
-  activity: Activity[];
-}
-
-export interface InitialState {
-  user: User | null;
-  todos: Todo[];
-  projects: Project[];
 }
