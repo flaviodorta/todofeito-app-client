8cd9bc299d868ef95084893cf399ad12ebe9fa32
flaviodorta <dorta.dev@gmail.com>
2022-08-19 11:42:52 -0300

    feat: add task component as shortcut in todos  items list

--- client/src/pages/Inbox/Inbox.tsx
+++ client/src/pages/Inbox/Inbox.tsx
@@ -5,6 +5,7 @@ import { useAppSelector } from '../../redux/store';
 import { EllipsisSolidIcon as EllipsisIcon } from '../../components/Icons/EllipsisSolid';
 import { SlidersSolidIcon as SlidersIcon } from '../../components/Icons/SlidersSolidIcon';
 import { MessageRegularIcon as MessageIcon } from '../../components/Icons/MessageRegularIcon';
+import { PlusSolidIcon as PlusIcon } from '../../components/Icons/PlusSolidIcon';
 
 import styled from 'styled-components';
 export const Container = styled.div`
@@ -59,6 +60,7 @@ export const Todos = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
+  margin-bottom: 2rem;
 `;
 
 export const Flex = styled.div<{ isSidebarOpen: boolean }>`
@@ -67,6 +69,48 @@ export const Flex = styled.div<{ isSidebarOpen: boolean }>`
   margin-top: 4.5rem;
   margin-left: ${(props) => (props.isSidebarOpen ? '10rem' : '0rem')};
   transition: margin-left 0.145s ease;
+`;
+
+export const AddTask = styled.div`
+  width: 100%;
+  height: 3rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  font-weight: 200;
+  padding: 1rem;
+  color: ${(props) => props.theme.colors.grey.three};
+  cursor: pointer;
+
+  svg {
+    width: 1.2rem;
+    height: 1.2rem;
+    fill: ${(props) => props.theme.colors.blue};
+  }
+
+  .icon-bg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 1rem;
+    margin-bottom: 0.2rem;
+    width: 1.6rem;
+    height: 1.6rem;
+    border-radius: 50%;
+    color: ${(props) => props.theme.colors.white.one};
+  }
+
+  &:hover {
+    color: ${(props) => props.theme.colors.blue};
+
+    .icon-bg {
+      background-color: ${(props) => props.theme.colors.blue};
+    }
+
+    svg {
+      fill: white;
+    }
+  }
 `;
 
 export function Inbox(): JSX.Element {
@@ -95,7 +139,12 @@ export function Inbox(): JSX.Element {
               </Options>
             </Heading>
             <Todos>todos</Todos>
-            <p>add task</p>
+            <AddTask>
+              <div className='icon-bg'>
+                <PlusIcon />
+              </div>
+              Add task
+            </AddTask>
             <p>add section (invisible withouthovet)</p>
           </Flex>
         </Container>
